d46fe51e5f6dd2e3e3b1f42d67caf70c324faa85
Vijay Mulsaniya <vijay.tutor@gmail.com>
2025-06-10 08:29:26 +0530

    Add HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import ReviewPage from './ReviewPage';
 import { Home } from './Home';
 
